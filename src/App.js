936f2fb35212def5debc4fbfe91c141460ff9474
Nidhi <138355491+nidhi752@users.noreply.github.com>
2024-10-06 14:17:47 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState } from 'react';
 import "./App.css";
 import WidgetGalleryModal from "./modals/WidgetGalleryModal";
 import Draggable from "react-draggable";
@@ -70,138 +70,103 @@ function App() {
           setWidgets={setWidgets}
         />
       )}
-      <div
-        className="add-button"
-        onClick={() => {
-          console.log("add button clicked");
-          setShowWidgetModal(true);
-          setSelectedWidgetArea("none-widget");
-        }}
-      >
-        <p
-          className="add-widget-button"
-          style={{
-            background: "none",
-            border: "none",
-            color: "black",
-            fontSize: "30px",
-            cursor: "pointer",
-            backgroundColor: "white",
-            borderRadius: "50%",
-            width: "50px",
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-            height: "50px",
-            position: "fixed",
-            bottom: "10px",
-            right: "10px",
-          }}
-        >
-          +
-        </p>
-      </div>
-
-      <div className="none-widget">
-        {widgets.length > 0 &&
-          widgets.map((widget, index) => {
-            if (widget.area === "none-widget") {
-              return (
-                <Draggable
-                  bounds={{
-                    left: 0,
-                    top: 0,
-                    right: window.innerWidth - 300,
-                    bottom: window.innerHeight - 300,
-                  }} // Adjust bounds
-                  key={widget.id}
-                >
-                  <div style={{ padding: 10 }} className="widget-container">
-                    <div style={{ marginBottom: 10 }}>
-                      <select
-                        value={widget.area}
-                        onChange={(e) => {
-                          let widgetList = [...widgets];
-                          widgetList[index].area = e.target.value;
-                          setWidgets(widgetList);
-                        }}
-                      >
-                        <option value="none-widget">Floating</option>
-                        <option value="left-widget">Left</option>
-                        <option value="main-widget">Main</option>
-                        <option value="right-widget">Right</option>
-                      </select>
-                      <button
-                        onClick={() => {
-                          removeWidget(widget.id);
-                        }}
-                        style={{
-                          background: "none",
-                          border: "none",
-                          color: "white",
-                          fontSize: "15px",
-                          cursor: "pointer",
-                          float: "right",
-                        }}
-                      >
-                        Close
-                      </button>
-                    </div>
-                    {widget.component}
-                  </div>
-                </Draggable>
-              );
-            }
-          })}
-      </div>
-
       <div className="App w-full h-screen">
-        <div className="container">
-          <div className="left-widget">
-            {widgets.length > 0 &&
-              widgets.map((widget, index) => {
-                if (widget.area === "left-widget") {
-                  return (
-                    <div style={{ padding: 10 }} className="widget-container">
-                      <div style={{ marginBottom: 10 }}>
-                        <select
-                          value={widget.area}
-                          onChange={(e) => {
-                            let widgetList = [...widgets];
-                            widgetList[index].area = e.target.value;
-                            setWidgets(widgetList);
-                          }}
-                        >
-                          <option value="none-widget">Floating</option>
-                          <option value="left-widget">Left</option>
-                          <option value="main-widget">Main</option>
-                          <option value="right-widget">Right</option>
-                        </select>
-                        <button
-                          onClick={() => {
-                            removeWidget(widget.id);
-                          }}
-                          style={{
-                            background: "none",
-                            border: "none",
-                            color: "white",
-                            fontSize: "15px",
-                            cursor: "pointer",
-                            float: "right",
-                          }}
-                        >
-                          Close
-                        </button>
+        <div className="dashboard-container">
+          {widgets.length > 0 &&
+            widgets.map((widget, index) => {
+              if (widget.area === "none-widget") {
+                return (
+                  <Draggable
+                    bounds="parent"
+                    key={widget.id}
+                    defaultPosition={{x: 0, y: 0}}
+                  >
+                    <div className="floating-widget-wrapper">
+                      <div style={{ padding: 10 }} className="widget-container">
+                        <div style={{ marginBottom: 10 }}>
+                          <select
+                            value={widget.area}
+                            onChange={(e) => {
+                              let widgetList = [...widgets];
+                              widgetList[index].area = e.target.value;
+                              setWidgets(widgetList);
+                            }}
+                          >
+                            <option value="none-widget">Floating</option>
+                            <option value="left-widget">Left</option>
+                            <option value="main-widget">Main</option>
+                            <option value="right-widget">Right</option>
+                          </select>
+                          <button
+                            onClick={() => {
+                              removeWidget(widget.id);
+                            }}
+                            style={{
+                              background: "none",
+                              border: "none",
+                              color: "white",
+                              fontSize: "15px",
+                              cursor: "pointer",
+                              float: "right",
+                            }}
+                          >
+                            Close
+                          </button>
+                        </div>
+                        {widget.component}
                       </div>
-                      {widget.component}
                     </div>
-                  );
-                }
-              })}
-            {widgets.filter((widget) => widget.area === "left-widget").length <
-              3 &&
-              widgets.filter((widget) => widget.area === "left-widget").length >
-                0 && (
+                  </Draggable>
+                );
+              }
+              return null;
+            })}
+          <div className="container">
+            <div className="left-widget">
+              {widgets.length > 0 &&
+                widgets.map((widget, index) => {
+                  if (widget.area === "left-widget") {
+                    return (
+                      <div style={{ padding: 10 }} className="widget-container">
+                        <div style={{ marginBottom: 10 }}>
+                          <select
+                            value={widget.area}
+                            onChange={(e) => {
+                              let widgetList = [...widgets];
+                              widgetList[index].area = e.target.value;
+                              setWidgets(widgetList);
+                            }}
+                          >
+                            <option value="none-widget">Floating</option>
+                            <option value="left-widget">Left</option>
+                            <option value="main-widget">Main</option>
+                            <option value="right-widget">Right</option>
+                          </select>
+                          <button
+                            onClick={() => {
+                              removeWidget(widget.id);
+                            }}
+                            style={{
+                              background: "none",
+                              border: "none",
+                              color: "white",
+                              fontSize: "15px",
+                              cursor: "pointer",
+                              float: "right",
+                            }}
+                          >
+                            Close
+                          </button>
+                        </div>
+                        {widget.component}
+                      </div>
+                    );
+                  }
+                })}
+              {widgets.filter((widget) => widget.area === "left-widget").length <
+                3 &&
+                widgets.filter((widget) => widget.area === "left-widget").length >
+                  0 && (
                 <button
                   className="add-widget-button"
                   onClick={() => {
@@ -212,108 +177,108 @@ function App() {
                   + Add Widget
                 </button>
               )}
-          </div>
+            </div>
 
-          <div className="main-widget">
-            {widgets.length > 0 &&
-              widgets.map((widget, index) => {
-                if (widget.area === "main-widget") {
-                  return (
-                    <div style={{ padding: 10 }} className="widget-container">
-                      <div style={{ marginBottom: 10 }}>
-                        <select
-                          value={widget.area}
-                          onChange={(e) => {
-                            let widgetList = [...widgets];
-                            widgetList[index].area = e.target.value;
-                            setWidgets(widgetList);
-                          }}
-                        >
-                          <option value="none-widget">Floating</option>
-                          <option value="left-widget">Left</option>
-                          <option value="main-widget">Main</option>
-                          <option value="right-widget">Right</option>
-                        </select>
-                        <button
-                          onClick={() => {
-                            removeWidget(widget.id);
-                          }}
-                          style={{
-                            background: "none",
-                            border: "none",
-                            color: "white",
-                            fontSize: "15px",
-                            cursor: "pointer",
-                            float: "right",
-                          }}
-                        >
-                          Close
-                        </button>
+            <div className="main-widget">
+              {widgets.length > 0 &&
+                widgets.map((widget, index) => {
+                  if (widget.area === "main-widget") {
+                    return (
+                      <div style={{ padding: 10 }} className="widget-container">
+                        <div style={{ marginBottom: 10 }}>
+                          <select
+                            value={widget.area}
+                            onChange={(e) => {
+                              let widgetList = [...widgets];
+                              widgetList[index].area = e.target.value;
+                              setWidgets(widgetList);
+                            }}
+                          >
+                            <option value="none-widget">Floating</option>
+                            <option value="left-widget">Left</option>
+                            <option value="main-widget">Main</option>
+                            <option value="right-widget">Right</option>
+                          </select>
+                          <button
+                            onClick={() => {
+                              removeWidget(widget.id);
+                            }}
+                            style={{
+                              background: "none",
+                              border: "none",
+                              color: "white",
+                              fontSize: "15px",
+                              cursor: "pointer",
+                              float: "right",
+                            }}
+                          >
+                            Close
+                          </button>
+                        </div>
+                        {widget.component}
                       </div>
-                      {widget.component}
-                    </div>
-                  );
-                }
-              })}
-            {widgets.filter((widget) => widget.area === "main-widget").length <
-              2 && (
-              <button
-                className="add-widget-button"
-                onClick={() => {
-                  setShowWidgetModal(true);
-                  setSelectedWidgetArea("main-widget");
-                }}
-              >
-                + Add Widget
-              </button>
-            )}
-          </div>
+                    );
+                  }
+                })}
+              {widgets.filter((widget) => widget.area === "main-widget").length <
+                2 && (
+                <button
+                  className="add-widget-button"
+                  onClick={() => {
+                    setShowWidgetModal(true);
+                    setSelectedWidgetArea("main-widget");
+                  }}
+                >
+                  + Add Widget
+                </button>
+              )}
+            </div>
 
-          <div className="right-widget">
-            {widgets.length > 0 &&
-              widgets.map((widget, index) => {
-                if (widget.area === "right-widget") {
-                  return (
-                    <div style={{ padding: 10 }} className="widget-container">
-                      <div style={{ marginBottom: 10 }}>
-                        <select
-                          value={widget.area}
-                          onChange={(e) => {
-                            let widgetList = [...widgets];
-                            widgetList[index].area = e.target.value;
-                            setWidgets(widgetList);
-                          }}
-                        >
-                          <option value="none-widget">Floating</option>
-                          <option value="left-widget">Left</option>
-                          <option value="main-widget">Main</option>
-                          <option value="right-widget">Right</option>
-                        </select>
-                        <button
-                          onClick={() => {
-                            removeWidget(widget.id);
-                          }}
-                          style={{
-                            background: "none",
-                            border: "none",
-                            color: "white",
-                            fontSize: "15px",
-                            cursor: "pointer",
-                            float: "right",
-                          }}
-                        >
-                          Close
-                        </button>
+            <div className="right-widget">
+              {widgets.length > 0 &&
+                widgets.map((widget, index) => {
+                  if (widget.area === "right-widget") {
+                    return (
+                      <div style={{ padding: 10 }} className="widget-container">
+                        <div style={{ marginBottom: 10 }}>
+                          <select
+                            value={widget.area}
+                            onChange={(e) => {
+                              let widgetList = [...widgets];
+                              widgetList[index].area = e.target.value;
+                              setWidgets(widgetList);
+                            }}
+                          >
+                            <option value="none-widget">Floating</option>
+                            <option value="left-widget">Left</option>
+                            <option value="main-widget">Main</option>
+                            <option value="right-widget">Right</option>
+                          </select>
+                          <button
+                            onClick={() => {
+                              removeWidget(widget.id);
+                            }}
+                            style={{
+                              background: "none",
+                              border: "none",
+                              color: "white",
+                              fontSize: "15px",
+                              cursor: "pointer",
+                              float: "right",
+                            }}
+                          >
+                            Close
+                          </button>
+                        </div>
+                        {widget.component}
                       </div>
-                      {widget.component}
-                    </div>
-                  );
-                }
-              })}
-            {widgets.filter((widget) => widget.area === "right-widget").length <
-              3 &&
-              widgets.filter((widget) => widget.area === "right-widget")
-                .length > 0 && (
+                    );
+                  }
+                })}
+              {widgets.filter((widget) => widget.area === "right-widget").length <
+                3 &&
+                widgets.filter((widget) => widget.area === "right-widget")
+                  .length > 0 && (
                 <button
                   className="add-widget-button"
                   onClick={() => {
@@ -324,6 +289,7 @@ function App() {
                   + Add Widget
                 </button>
               )}
+            </div>
           </div>
         </div>
       </div>
